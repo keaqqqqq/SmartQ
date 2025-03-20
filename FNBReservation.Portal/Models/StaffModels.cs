20af7e5d30eedec0f2023872c02cd455807c7636
ronrayOng <153099324+ronrayOng@users.noreply.github.com>
2025-03-20 23:20:04 +0800

    if want can bye bye this version

---
+++ FNBReservation.Portal/Models/StaffModels.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FNBReservation.Portal.Models
+{
+    public class StaffMember
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Name must be between 2 and 100 characters.", MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Username must be between 5 and 50 characters.", MinimumLength = 5)]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Phone]
+        [StringLength(15, ErrorMessage = "Phone number must be between 8 and 15 characters.", MinimumLength = 8)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        public int OutletId { get; set; }
+
+        public Outlet Outlet { get; set; }
+    }
+
+    public class Outlet
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Name must be between 2 and 100 characters.", MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(255, ErrorMessage = "Address must be between 5 and 255 characters.", MinimumLength = 5)]
+        public string Address { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+
+    public class StaffCreateRequest
+    {
+        [Required]
+        [StringLength(100, ErrorMessage = "Name must be between 2 and 100 characters.", MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Username must be between 5 and 50 characters.", MinimumLength = 5)]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Phone]
+        [StringLength(15, ErrorMessage = "Phone number must be between 8 and 15 characters.", MinimumLength = 8)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Password must be between 8 and 50 characters.", MinimumLength = 8)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public int OutletId { get; set; }
+    }
+
+    public class StaffUpdateRequest
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Name must be between 2 and 100 characters.", MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Username must be between 5 and 50 characters.", MinimumLength = 5)]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Phone]
+        [StringLength(15, ErrorMessage = "Phone number must be between 8 and 15 characters.", MinimumLength = 8)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+
+        [StringLength(50, ErrorMessage = "Password must be between 8 and 50 characters.", MinimumLength = 8)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
--- FNBReservation.Portal/Program.cs
+++ FNBReservation.Portal/Program.cs
@@ -1,32 +1,55 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MudBlazor;
 using MudBlazor.Services;
-using FNBReservation.Portal.Components;
+using FNBReservation.Portal.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents()
-    .AddInteractiveServerComponents();
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
 
+// Add MudBlazor services
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomRight;
+    config.SnackbarConfiguration.PreventDuplicates = true;
+    config.SnackbarConfiguration.NewestOnTop = true;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+    config.SnackbarConfiguration.VisibleStateDuration = 5000;
+    config.SnackbarConfiguration.HideTransitionDuration = 500;
+    config.SnackbarConfiguration.ShowTransitionDuration = 500;
+});
 
-builder.Services.AddMudServices();
+// Configure HttpClient
+builder.Services.AddHttpClient();
+
+// Register Application Services
+builder.Services.AddScoped<IStaffService, StaffService>();
+builder.Services.AddScoped<IOutletService, OutletService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-app.UseAntiforgery();
 
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
 
 app.Run();
