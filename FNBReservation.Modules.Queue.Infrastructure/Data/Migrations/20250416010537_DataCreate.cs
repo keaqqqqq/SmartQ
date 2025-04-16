e99ad0ce2ffd0a9c2963739fa976094e8736b75a
ronrayOng <153099324+ronrayOng@users.noreply.github.com>
2025-04-16 09:24:18 +0800

    data migration

---
+++ FNBReservation.Modules.Queue.Infrastructure/Data/Migrations/20250416010537_DataCreate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FNBReservation.Modules.Queue.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
